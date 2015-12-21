519ebb52c36b548a5d78dacb131a9374ea186ac6
turboflanagan <turboflanagan@gmail.com>
2015-12-21 18:57:51 -0500

    add date

--- controller.js
+++ controller.js
@@ -4,7 +4,12 @@ myApp.controller('myController', function ($scope, $http){
 	$http.get('http://localhost:3001').then(function (theData){
 		$scope.weatherJSON = theData.data;
 
-		
-		
-	});
+	var date = new Date($scope.weatherJSON.dt);
+	var sunrise = new Date($scope.weatherJSON.sys.sunrise);
+	console.log($scope.weatherJSON.dt);
+	console.log($scope.weatherJSON.sys.sunrise);
+
+	})
+
 });
+
