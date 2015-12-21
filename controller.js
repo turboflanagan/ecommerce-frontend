6fcc2d5f7c586fbca70bbb958c016585c5d6478d
turboflanagan <turboflanagan@gmail.com>
2015-12-21 15:42:29 -0500

    send JSON to page

--- controller.js
+++ controller.js
@@ -1,3 +1,10 @@
-angular.module('myApp', []).controller('myController', function($scope){
+var myApp=angular.module('myApp', []);
 
+myApp.controller('myController', function ($scope, $http){
+	$http.get('http://localhost:3001').then(function (theData){
+		$scope.weatherJSON = theData.data;
+
+		
+		
+	});
 });
